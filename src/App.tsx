9f83a07d935b2539d3f373764971a5ec2c355bf0
Pablo Jara <pablojaramunoz@icloud.com>
2025-06-06 03:23:40 -0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
 import PageHeader from './components/PageHeader/PageHeader'
 import UserList from './pages/UserList/UserList'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import UserDetail from './pages/UserDetail/UserDetail'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient();
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
       <PageHeader />
-      <UserList />
-    </>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+      </Routes>
+    </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,32 +1,33 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
-
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function UserCard({ user }: { user: any }) {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                        {user.name}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {user.username}
-                    </Typography>
-                    <Typography gutterBottom variant="body1" component="div">
-                        {user.phone}
-                    </Typography>
-                    <Typography gutterBottom variant="body1" component="div">
-                        {user.email}
-                    </Typography>
-                    <Typography gutterBottom variant="body1" component="i">
-                        {user.address.city}
-                    </Typography>
-                    <Typography gutterBottom variant="body1" component="div">
-                        {user.company.name}
-                    </Typography>
-
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <Link to={`/user/${user.id}`} style={{ textDecoration: 'none' }}>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h6" component="div">
+                            {user.name}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {user.username}
+                        </Typography>
+                        <Typography gutterBottom variant="body1" component="div">
+                            {user.phone}
+                        </Typography>
+                        <Typography gutterBottom variant="body1" component="div">
+                            {user.email}
+                        </Typography>
+                        <Typography gutterBottom variant="body1" component="i">
+                            {user.address.city}
+                        </Typography>
+                        <Typography gutterBottom variant="body1" component="div">
+                            {user.company.name}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
     )
 }
