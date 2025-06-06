4a4cf7c48e88a495a7629b407238da070523cf37
Pablo Jara <pablojaramunoz@icloud.com>
2025-06-06 02:15:37 -0300

    adding material ui deps

---
+++ src/components/UserTable/UserTable.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import UserCard from "../UserCard/UserCard";
+
+export default function UserTable() {
+    return (
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {Array.from(Array(6)).map((_, index) => (
+                <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+                    <UserCard />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
---
+++ src/pages/UserList/UserList.tsx
@@ -0,0 +1,10 @@
+import UserTable from "../../components/UserTable/UserTable"
+
+export default function UserList() {
+    return (
+        <div>
+            <UserTable />
+
+        </div>
+    )
+}
