f9a09d88474412a9d9ddcd4db7dae5fb16cfe3d3
Pablo Jara <pablojaramunoz@icloud.com>
2025-06-09 12:36:37 -0300

    fix ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import './App.css'
 import PageHeader from './components/PageHeader/PageHeader'
 import UserList from './pages/UserList/UserList'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import UserDetail from './pages/UserDetail/UserDetail'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-const queryClient = new QueryClient();
-function App() {
 
+const queryClient = new QueryClient();
+
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <PageHeader />
-      <Routes>
-        <Route path="/" element={<UserList />} />
-        <Route path="/user/:id" element={<UserDetail />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/tsm-challenge">
+        <PageHeader />
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/user/:id" element={<UserDetail />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   )
 }
